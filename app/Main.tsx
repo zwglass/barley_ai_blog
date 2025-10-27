595c9b868e148572a05f5bc651255435f0fcba99
sen <senmalay@icloud.com>
2025-10-27 15:20:31 +0800

    update eslint2

--- app/Main.tsx
+++ app/Main.tsx
@@ -57,7 +57,9 @@ export default function Home({ posts }: HomeProps) {
                             </Link>
                           </h2>
                           <div className="flex flex-wrap">
-                            {tags?.map((tag) => <Tag key={tag} text={tag} />)}
+                            {tags?.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
                           </div>
                         </div>
                         <div className="prose max-w-none text-gray-500 dark:text-gray-400">
